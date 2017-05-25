66292d5a69056e89a7f0c5e4790fff87e5ea95ba
Michał Kordulewski <michal@kordulewski.pl>
2017-05-25 20:42:55 +0200

    Hello component

--- js/examples/hello.js
+++ js/examples/hello.js
@@ -2,7 +2,7 @@
 
 var _jsxFileName = '_js/examples/hello.js';
 var name = Math.random() > 0.5 ? 'nieznajomy' : 'internauto';
-var HELLO_COMPONENT = ('\nclass HelloMessage extends React.Component {\n  render() {\n    return <div>Witaj, {this.props.name}</div>;\n  }\n}\n\nReactDOM.render(<HelloMessage name="' + name + '" />, mountNode);\n').trim();
+var HELLO_COMPONENT = ('\nclass HelloMessage extends React.Component {\n  render() {\n    return <div>Witaj, {this.props.name}.</div>;\n  }\n}\n\nReactDOM.render(<HelloMessage name="' + name + '" />, mountNode);\n').trim();
 
 ReactDOM.render(React.createElement(ReactPlayground, { codeText: HELLO_COMPONENT, __source: {
     fileName: _jsxFileName,
